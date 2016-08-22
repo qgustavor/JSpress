c7eba6a0a4bcea3d8ffe8278d587b1a005e15640
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2016-08-22 08:59:46 -0300

    Simplify and normalize whitespace
    * Remove fs dependency (it's I/O not compression)
    * Remove console
    * Remove ratio calculation

--- compress.js
+++ compress.js
@@ -1,27 +1,18 @@
-var fs = require('fs');
-var file = '//**FILE TO BE COMPRESSED**//'
-var contents = fs.readFileSync(file).toString();
-var text = contents.trim();
-var wordInText = text.split(" ");
-var compressionArray = [];
-var originalLength = text.length;
-
-
-for (var i = 0; i < wordInText.length; i++){
-	percent = (i/wordInText.length) * 100;
-	console.log(percent);
-     if(compressionArray.indexOf(wordInText[i]) < 0){
-     	compressionArray.push(wordInText[i]);
-     } else {
-     	compressionArray.push("*" + wordInText.indexOf(wordInText[i]).toString(36));
-     }
+function compress(contents) {
+	var text = contents.trim();
+	var wordInText = text.split(" ");
+	var compressionArray = [];
+	var originalLength = text.length;
+	
+	for (var i = 0; i < wordInText.length; i++){
+		if(compressionArray.indexOf(wordInText[i]) < 0){
+			compressionArray.push(wordInText[i]);
+		} else {
+			compressionArray.push("*" + wordInText.indexOf(wordInText[i]).toString(36));
+		}
+	}
+	
+	return compressionArray.join(" ");
 }
 
-console.log("ratio: " + originalLength / compressionArray.join().length)
-
-fs.writeFile(file + ".FWAD", compressionArray.join(" "), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-  });
+module.exports = compress;
