8927f6ed145fe81996745589cfa8ab880645e7c3
Gustavo Rodrigues <qgustavor@users.noreply.github.com>
2016-08-22 08:59:43 -0300

    Simplify and normalize whitespace
    * Remove fs dependency (it's I/O not compression)
    * Remove console

--- decompress.js
+++ decompress.js
@@ -1,26 +1,21 @@
-var fs = require('fs');
-var file = '//**FILE TO BE DECOMPRESSED**//'
-var contents = fs.readFileSync(file).toString();
-var text = contents.trim();
-var wordInText = text.split(" ");
-decompressionArray = [];
-
-for (var i = 0; i < wordInText.length; i++) {
-  percent = (i/wordInText.length) * 100;
-  console.log(percent);
-     if(wordInText[i][0] == "*"){
+function decompress(contents) {
+  var text = contents.trim();
+  var wordInText = text.split(" ");
+  var decompressionArray = [];
+  
+  for (var i = 0; i < wordInText.length; i++) {
+    percent = (i / wordInText.length) * 100;
+      if(wordInText[i][0] == "*"){
       var D = wordInText[i].slice(1);
       var ind = parseInt(D, 36);
-      word = wordInText[ind];
+      var word = wordInText[ind];
       decompressionArray.push(word);
-     } else {
+    } else {
       decompressionArray.push(wordInText[i]);
-     }
+    }
+  }
+  
+  return decompressionArray.join(" ");
 }
 
-fs.writeFile(file , decompressionArray.join(" "), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-  });
+module.exports = decompress;
